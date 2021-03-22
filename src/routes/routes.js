68083f8aa04c62a996b355032ac463346d6a112f
AngelVillaDev <villalopezluis99@gmail.com>
2021-03-22 14:28:30 -0700

    API complete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,34 +11,127 @@ router.get('/', (req, res) => {
 });
 
 **/
-const telas = require('./telas.json');
-console.log(telas);
+const pedidos = require('./pedidos.json');
+console.log(pedidos);
 
 router.get('/', (req,res) => {
-    res.json(telas);
+    res.json(pedidos);
 })
+
+// Get por id
 
 router.get('/:id', (req,res) => {
     const {id}= req.params;
-    telas.forEach(tela => {
-        if(tela.id == id){
-        res.json(tela);
-            console.log(tela.colorTela);
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){ 
+        res.json(pedido);
         }
     });
     //const id = req.params.id;
-    console.log(id);
-})
+});
+
+//Get por cantidades
+
+router.get('/:id/cantidadTelaMetros/', (req,res) => {
+    const {id}= req.params;
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){
+        res.json(pedido.cantidadTelaMetros);
+        }
+    });
+});
+
+router.get('/:id/cantidadAgujetasPares/', (req,res) => {
+    const {id}= req.params;
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){
+        res.json(pedido.cantidadAgujetasPares);
+        }
+    });
+});
+
+router.get('/:id/cantidadSuelas/', (req,res) => {
+    const {id}= req.params;
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){
+        res.json(pedido.cantidadSuelas);
+        }
+    });
+});
+
+router.get('/:id/cantidadZapatos/', (req,res) => {
+    const {id}= req.params;
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){
+        res.json(pedido.cantidadZapatos);
+        }
+    });
+});
+
+//Get por colores
+
+router.get('/:id/colorTela/', (req,res) => {
+    const {id}= req.params;
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){
+        res.json(pedido.colorTela);
+        }
+    });
+});
+
+router.get('/:id/colorAgujetas/', (req,res) => {
+    const {id}= req.params;
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){
+        res.json(pedido.colorAgujetas);
+        }
+    });
+});
+
+router.get('/:id/colorSuelas/', (req,res) => {
+    const {id}= req.params;
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){
+        res.json(pedido.colorSuelas);
+        }
+    });
+});
+
+//Get por costos
+
+router.get('/:id/costoPorZapatos/', (req,res) => {
+    const {id}= req.params;
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){
+        res.json(pedido.costoPorZapatos);
+        }
+    });
+});
+
+router.get('/:id/costoTotal/', (req,res) => {
+    const {id}= req.params;
+    pedidos.forEach(pedido => {
+        if(pedido.id == id){
+        const costoTotalPedido = pedido.costoPorZapatos * pedido.cantidadZapatos;
+        res.json({"costoTotal": costoTotalPedido});
+        }
+    });
+});
+
+
+//Post General
 
 router.post('/', (req,res) => {
-    const {colorTela, cantidadTelaMetros} = req.body;
+    const {cantidadTelaMetros, cantidadAgujetasPares, cantidadSuelas, cantidadZapatos, colorTela,
+        colorAgujetas, colorSuelas, costoPorZapatos} = req.body;
 
-    if(colorTela && cantidadTelaMetros){
+    if(cantidadTelaMetros && cantidadAgujetasPares && cantidadSuelas && cantidadZapatos && colorTela
+        && colorAgujetas && colorSuelas && costoPorZapatos){
         const id = telas.length +1;
-        const nuevaTela = {...req.body, id}
-        telas.push(nuevaTela);
+        const nuevoPedido = {...req.body, id}
+        telas.push(nuevoPedido);
         //console.log(nuevaTela);
-        res.status(200).json(telas);
+        res.status(200).json(pedidos);
     }
     else{
         res.status(500).json({error:"no data"});
